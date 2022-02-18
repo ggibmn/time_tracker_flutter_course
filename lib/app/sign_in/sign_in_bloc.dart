e5cf51f63a5af94b2e0a63765c2e3a9e567b6d67
ggibmn <baybolovamaral@gmail.com>
2022-02-18 18:46:19 +0600

    provider&consumer

--- lib/app/sign_in/sign_in_bloc.dart
+++ lib/app/sign_in/sign_in_bloc.dart
@@ -1,7 +1,13 @@
 import 'dart:async';
 
+import 'package:time_tracker_flutter_course/services/auth.dart';
+
 class SignInBloc {
+  final AuthBase auth;
+
   final StreamController<bool> _isLoadingController = StreamController<bool>();
+
+  SignInBloc(this.auth);
 
   Stream<bool> get isLoadingStream => _isLoadingController.stream;
 
--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -9,13 +9,19 @@ import 'package:time_tracker_flutter_course/common_widgets/show_exception_alert_
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class SignInPage extends StatelessWidget {
+  final SignInBloc bloc;
+
+  const SignInPage({Key? key, required this.bloc}) : super(key: key);
+
   static Widget create(BuildContext context) {
     return Provider<SignInBloc>(
       create: (_) => SignInBloc(),
-      child: SignInPage(),
+      dispose: (_, bloc) => bloc.dispose(),
+      child: Consumer<SignInBloc>(
+        builder: (_, bloc, __) => SignInPage(bloc: bloc),
+      ),
     );
   }
-
 
   void _showSignInError(BuildContext context, Exception exception) {
     if (exception is FirebaseException &&
@@ -27,7 +33,6 @@ class SignInPage extends StatelessWidget {
   }
 
   Future<void> _signInAnonymously(BuildContext context) async {
-    final bloc = Provider.of<SignInBloc>(context, listen: false);
     try {
       bloc.setIsLoading(true);
       final auth = Provider.of<AuthBase>(context, listen: false);
@@ -40,7 +45,6 @@ class SignInPage extends StatelessWidget {
   }
 
   Future<void> _signInWithGoogle(BuildContext context) async {
-    final bloc = Provider.of<SignInBloc>(context, listen: false);
     final auth = Provider.of<AuthBase>(context, listen: false);
     try {
       bloc.setIsLoading(true);
@@ -53,7 +57,6 @@ class SignInPage extends StatelessWidget {
   }
 
   Future<void> _signInWithFacebook(BuildContext context) async {
-    final bloc = Provider.of<SignInBloc>(context, listen: false);
     final auth = Provider.of<AuthBase>(context, listen: false);
     try {
       bloc.setIsLoading(true);
@@ -76,7 +79,6 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final bloc = Provider.of<SignInBloc>(context, listen: false);
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
